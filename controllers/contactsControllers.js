5396fd032cc702b268fdfccbde9811088b89a27a
Your Name <alexandra.mukhamedova@gmail.com>
2025-09-10 13:50:45 +0100

    HW4

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,65 @@
+import {
+  updateContactById,
+  getContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await getContacts();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  try {
+    const contact = await getContactById(req.params.id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await removeContact(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const contact = await addContact(req.body);
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await updateContactById(id, req.body);
+    if (!contact) {
+      throw HttpError(404);
+    }
+
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
